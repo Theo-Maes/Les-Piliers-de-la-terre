5d810a1bc3dc6eb33170028ce2f6b7ea5516d41e
Alan Grenet <ga200662@etu.univ-lehavre.fr>
2021-06-07 10:31:49 +0200

    class pilier premier test

---
+++ Pilier.java
@@ -0,0 +1,27 @@
+public class Pilier
+{
+	private int x;
+	private int y;
+	private char coul;
+	
+	private static int nbPilier = 0;
+	 
+	
+	public Pilier(){this(0,0);}
+	
+	public Pilier(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		
+		if( ++Pilier.nbPilier%2 == 0)this.coul = 'G';
+		else this.coul = 'M';
+	}
+	
+	public char getCoul(){return this.coul;}
+	
+	public String toString()
+	{
+		return this.coul + "(" + this.x + "," + this.y + ")";
+	}
+}
