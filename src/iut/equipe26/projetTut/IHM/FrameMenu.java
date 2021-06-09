5817f6960e4d83ec2b16b1cc7398ac00a350b3f9
TooikLeChevalier <57256410+TooikLeChevalier@users.noreply.github.com>
2021-06-09 08:32:01 +0200

    Update FrameMenu.java

--- src/iut/equipe26/projetTut/IHM/FrameMenu.java
+++ src/iut/equipe26/projetTut/IHM/FrameMenu.java
@@ -9,7 +9,7 @@ public class FrameMenu extends JFrame
 		this.setTitle("Menu - Les Pilliers de la Terre");
 		this.setSize(700, 600);
 
-		this.add(new PanelChoix(this) );
+		this.add(new PanelChoixMenu(this) );
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
