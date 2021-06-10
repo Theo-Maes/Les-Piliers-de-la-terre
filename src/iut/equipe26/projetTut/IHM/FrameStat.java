d553629bb064bac10a641cce4bd26c06eb2d4b39
Paul Rayer <paulrayer61@gmail.com>
2021-06-10 15:35:05 +0200

    Création FrameStat

---
+++ src/iut/equipe26/projetTut/IHM/FrameStat.java
@@ -0,0 +1,34 @@
+package iut.equipe26.projetTut.IHM;
+
+import iut.equipe26.projetTut.Controleur;
+import iut.equipe26.projetTut.metier.Joueur;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+public class FrameStat extends JFrame
+{
+	private Joueur j;
+	private PanelStat panelStat;
+	
+	public FrameStat(Joueur j)
+	{
+		this.j = j;
+		this.setTitle("Statistique de " + j.getNom() );
+		this.setLocation(50,50);
+		this.setSize(300, 600);
+		
+		
+		this.panelStat = new PanelStat(j);
+		
+		this.add(this.panelStat);
+		
+		//activation
+		this.addWindowListener(Controleur.getInstance());
+
+		//autres
+		this.setUndecorated(true);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+}
